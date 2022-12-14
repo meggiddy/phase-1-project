a6a915fad2dac4778958b00a99615b9f0bf30a12
meggidy <mimamitchelle@gmail.com>
2022-12-14 10:59:29 +0300

    compiling Js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-window.onload = ()=> {
-  const song_img_el = document.getElementById("song-image")
-  const song_title_el = document.getElementById("song-title")
-  const song_artist_el = document.getElementById("song-artist")
-  const song_next_up_el = document.getElementById("song-next-up")
-  
-  const play_btn = document.getElementById("play-btn")
-  const play_btn_icon = document.getElementById("play-icon")
-  const prev_btn = document.getElementById("prev-btn")
-  const next_btn = document.getElementById("next-btn")
+window.onload = () => {
+  const song_img_el = document.getElementById("song-image");
+  const song_title_el = document.getElementById("song-title");
+  const song_artist_el = document.getElementById("song-artist");
+  const song_next_up_el = document.getElementById("song-next-up");
 
-  const audio_player = document.getElementById("music-player")
+  const play_btn = document.getElementById("play-btn");
+  const play_btn_icon = document.getElementById("play-icon");
+  const prev_btn = document.getElementById("prev-btn");
+  const next_btn = document.getElementById("next-btn");
+
+  const audio_player = document.getElementById("music-player");
   let current_song_index;
   let next_song_index;
 
@@ -18,12 +18,17 @@ window.onload = ()=> {
       title: "Song 1",
       artist: "Artist 1",
       img_path: "image/",
-      song_path: "/audio/"
+      song_path: "/audio/",
     },
-    {}
-  ]
+    {},
+  ];
 
-  InitPlayer()
+  play_btn.addEventListener("click", TogglePlaySong);
+  next_btn.addEventListener("click", ()=>ChangeSong());
+  prev_btn.addEventListener("click", ()=>ChangeSong(false));//passes false to next
+  
+  
+  InitPlayer();
   function InitPlayer() {
     current_song_index = 0;
     next_song_index = current_song_index + 1;
@@ -32,9 +37,53 @@ window.onload = ()=> {
 
   function updatePlayer() {
     let song = songs[current_song_index];
-  }
-} 
 
+    song_img_el.style = "background-image: url('" + song.img_path + "')";
+    song_title_el.innerHTML = song.title;
+    song_artist_el.innerHTML = song.artist;
+    song_next_up_el.innerHTML =
+      songs[next_song_index].title + "by" + songs[next_song_index].artist;
+
+    audio_player.src = song.song_path;
+  }
+
+  function TogglePlaySong() {
+    if (audio_player.paused) {
+      audio_player.play();
+      play_btn_icon.classList.remove("fa-play");
+      play_btn_icon.classList.add("fa-pause");
+    } else {
+      audio_player.pause();
+      play_btn_icon.classList.remove("fa-pause");
+      play_btn_icon.classList.add("fa-play");
+    }
+  }
+
+  function ChangeSong(next = true) {
+    //check if next songs are within the song limit
+    if (next) {
+      current_song_index++;
+      next_song_index = current_song_index + 1;
+      if (current_song_index > songs.length - 1) {
+        current_song_index = 0;
+        next_song_index = current_song_index + 1;
+      }
+      if (next_song_index > songs.length - 1) {
+        next_song_index = 0;
+      }
+    } else {
+      current_song_index--;
+      next_song_index = current_song_index + 1;
+      if (current_song_index < 0) {
+        current_song_index = songs.length - 1;
+        next_song_index = 0;
+      }
+    }
+
+    updatePlayer();
+    TogglePlaySong();
+  }
+};
 
 /* const auth_url =
   //this encases the user details for log in and playlist libraries
